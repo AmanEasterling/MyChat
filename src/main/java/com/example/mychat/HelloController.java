ee807ff07a9b539b4fbfa80bf2cd8beed8605dd6
amaneasterling <aeasterling26@scienceleadership.org>
2026-05-11 14:38:12 -0400

    Integrate ClientServer code.

--- src/main/java/com/example/mychat/HelloController.java
+++ src/main/java/com/example/mychat/HelloController.java
@@ -1,14 +1,32 @@
 package com.example.mychat;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    TextArea typeMessage;
+    ChoiceBox directMessage;
+    Button sendButton;
+    ListView chatBox;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+        System.out.println("CONNECT TO SERVER");
     }
+
+    public void addSelfAsChatter() {
+        System.out.println("Identify yourself to Server");
+    }
+
+    public void sendMessage() {
+        System.out.println("SENT MESSAGE TO ???");
+    }
+
+    public void receiveMessage() {
+
+    }
+
+    public void addAnotherChatter() {
+
+    }
+
 }
